50c6e59cdf68acb2dd44733ade2a97eadc1ca9a5
haphan217 <s3695302@rmit.edu.vn>
2021-08-01 20:43:31 +0700

    feat: setup FE

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "assets/demo/demo.css";
+import "assets/scss/paper-dashboard.scss?v=1.3.0";
+import "perfect-scrollbar/css/perfect-scrollbar.css";
+import AdminLayout from "./layouts/Admin.js";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
+          <Redirect to="/admin/dashboard" />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
