e32a0a135fb394ff6e23362b240073a90615864e
nguyenhn1299 <47587052+nguyenhn1299@users.noreply.github.com>
2021-09-19 08:43:35 +0700

    chore: summary API

--- backend/app/crud/crud_career.py
+++ backend/app/crud/crud_career.py
@@ -4,7 +4,8 @@ from pydantic import BaseModel  # pylint: disable=no-name-in-module
 
 from app.db.postgres import models
 from .crud_base import CRUDBase
-
+from sqlalchemy.orm import Session
+from sqlalchemy import func
 
 class CareerCreate(BaseModel):
     career_path: str
@@ -18,7 +19,8 @@ class CareerUpdate(BaseModel):
 
 
 class CRUDCareer(CRUDBase[models.Career, CareerCreate, CareerUpdate]):
-    pass
+    def get_career_count(self, db: Session):
+        return db.query(self.model).count()
 
 
 career = CRUDCareer(models.Career)
